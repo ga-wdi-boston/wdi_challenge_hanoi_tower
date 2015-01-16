81894eafb28d66ec183280ce0a55255131741d62
Daniel Tyson <danielctyson@gmail.com>
2015-01-16 12:16:52 -0500

    Used psuedocode to make recursive Hanoi solution

--- lib/tower_of_hanoi.rb
+++ lib/tower_of_hanoi.rb
@@ -1,1 +1,17 @@
-# your code here
+
+
+def move(num_disks, start, target, using)
+  if num_disks == 1
+    target << start.pop
+  else
+    move(num_disks - 1, start, using, target)
+    target << start.pop
+    move(num_disks - 1, using, target, start)
+  end
+end
+
+
+def solve_hanoi_tower(a, b, c)
+  move(a.length, a, c, b)
+  [a, b, c]
+end
